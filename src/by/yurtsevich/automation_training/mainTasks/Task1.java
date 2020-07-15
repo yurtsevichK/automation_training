1aac43bd989505b438289980bbc5f160e7aba212
yurtsevichK <nicecurls@icloud.com>
2020-07-15 11:56:26 +0300

    Initial commit

---
+++ src/by/yurtsevich/automation_training/mainTasks/Task1.java
@@ -0,0 +1,11 @@
+package by.yurtsevich.automation_training.mainTasks;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println("hello " + userName);
+    }
+}
